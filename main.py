520339b64d3536a4f8e67da3bc9b4808c41cd6a0
Vorlogg <vorlog03@gmail.com>
2021-05-16 20:48:14 +0300

    0.1

--- main.py
+++ main.py
@@ -5,15 +5,78 @@ import statistics
 from math import sqrt
 
 
-def read_txt(file: str) -> list[str]:
+def read_txt(file: str) -> list[list[str]]:
     rezult = []
     with open(file, "r", encoding="utf-8") as f:
         for i in f:
-            data=re.sub(r"[\n,?,–,…,.,!,«,»,—,\-,*,\t,\xa0-]|(\[.+\])|(<.+>)|(\(.+\))","",i)
-            if data:
-                rezult.append(data)
+            data = re.sub(r"[\n,?,–,…,.,!,«,»,—,\-,*,\t,\xa0-]|(\[.+\])|(<.+>)|(\(.+\))", "", i)
+            rez = data.lower().split()
+            if rez:
+                rezult.append(rez)
     return rezult
 
+
+def count_word(word: str, corp: list[list[str]]):
+    rez = 0
+    for i in corp:
+        rez += i.count(word.lower())
+    return rez
+
+
+def all_count_word(corp: list[list[str]]):
+    rez = 0
+    for i in corp:
+        rez += len(i)
+    return rez
+
+
+def count_word2(word1: str, word2: str, corp: list[list[str]]):
+    rez = 0
+    for i in corp:
+        for j in range(len(i)):
+            if word1 == i[j - 1] and word2 == i[j]:
+                rez += 1
+    return rez
+
+
+def p_word(word: str, corp: list[list[str]]):
+    return count_word(word, corp) / all_count_word(corp)
+
+
+def p_count_word2(word1: str, word2: str, corp: list[list[str]]):
+     rez= count_word2(word2, word1, corp) / count_word(word2,corp)
+    if rez==0:
+        rez=1
+    return rez
+
+
+test_word = "Пушкин.txt"
+# print(find_word(test_word, corp_text, 1))
+corp = read_txt(test_word)
+corp=[["this","is","the","malt"],["that","lay","in","the","house","that","jack","built"]]
+# corp=read_txt("1.txt")
+print(corp)
+print(all_count_word(corp))
+# w1=count_word("that", corp)
+# print(w1)
+# w2=p_count_word2("that", "jack", corp)
+# print(count_word2("that", "jack", corp))
+# print(w2)
+# rez1=count_word2("that", "jack", corp)/count_word("that", corp)
+# print(rez1)
+
+w1=p_word("this", corp)
+print(count_word2("is", "this", corp))
+w2=p_count_word2("is", "this", corp)
+
+w3=p_count_word2("is", "this", corp)
+w4=p_count_word2("house", "the", corp)
+print(w1)
+print(w2)
+print(w3)
+print(w4)
+rez=w1*w2*w3*w4
+print(rez)
 
 def find_word(word: str, text: dict, errors: int) -> Union[list[str], str]:
     """Поиск слов"""
@@ -37,9 +100,9 @@ def find_word(word: str, text: dict, errors: int) -> Union[list[str], str]:
     return rez
 
 
-def method_hamming_distance(word1: str, word2: str) -> int:
+def method_hamming_distance(word1: str, count_word2: str) -> int:
     rez = 0
-    for char1, char2, in zip(word1, word2):
+    for char1, char2, in zip(word1, count_word2):
         if char1 != char2:
             rez += 1
     return rez
@@ -79,12 +142,9 @@ def mean_squared_error(answer: list, input: list):
     mean_error = sum_error / float(len(y))
     return mean_error
 
-
-test_word = "Пушкин.txt"
-# print(find_word(test_word, corp_text, 1))
-print(read_txt(test_word))
-
-
+# test_word = "Пушкин.txt"
+# # print(find_word(test_word, corp_text, 1))
+# print(read_txt(test_word))
 
 # if __name__ == '__main__':
 #     print("Введите название файла книгу")
