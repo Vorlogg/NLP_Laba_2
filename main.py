adcc0f7c28efbbfbd4a5200c5b2444fe07c8ce47
Vorlogg <vorlog03@gmail.com>
2021-05-28 20:41:44 +0300

    генерит текст

--- main.py
+++ main.py
@@ -22,34 +22,34 @@ NGRAM = 4
 def read_txt(file: str):
     rezult_pr = []
     rezult_word = []
-    all_words=[]
+    all_words = []
     with open(file, "r", encoding="utf-8") as f:
         for i in f:
             data = re.sub(r"[\n,?,–,:,L=;,…,.,!,«,»,—,\-,*,\t,\xa0-]|(\[.+\])|(<.+>)|(\(.+\))", "", i)
             rez = data.lower()
             if rez:
                 if rez != "  " and rez != " " and rez != "   " and rez != "    ":
-                    #подсчет уникальных слов
-                    words=rez.split()
+                    # подсчет уникальных слов
+                    words = rez.split()
                     [all_words.append(i) for i in words]
-                    #разбитие на предложения
+                    # разбитие на предложения
                     rezult_pr.append(rez)
-                    #разбитие на токены и добавление $
+                    # разбитие на токены и добавление $
                     rez = "$ " * (NGRAM - 1) + rez + " $" * (NGRAM - 1)
                     rezult_words = rez.split()
                     rezult_word.append(rezult_words)
 
     # подсчет уникальных слов
-    all_words=set(all_words)
-    all_count_words=len(all_words)
-    return rezult_pr, rezult_word,all_words,all_count_words
+    all_words = set(all_words)
+    all_count_words = len(all_words)
+    return rezult_pr, rezult_word, all_words, all_count_words
 
 
 def n_gram(corp: list[list[str]]):
     rez = []
     for pr in corp:
         for ngrams in zip(*[pr[i:] for i in range(NGRAM)]):
-            rez.append(ngrams)
+            rez.append(list(ngrams))
     return rez
 
 
@@ -57,36 +57,40 @@ def n_gram(corp: list[list[str]]):
 # беру 2 последних слова и ищу в нграм
 # 'моих', 'стихов', 'ты') для ты считаем вероятность
 
-def find_word_count(word: str,ngram):
+def find_word_count(word: str, ngram):
     word_n = ["$"] * (NGRAM - 1)
     words = word.lower().split()
-    word_dickt=defaultdict(lambda:0.0)
+    word_dickt = defaultdict(lambda: 0.0)
     for w in words:
         word_n.append(w)
     rez = []
     for ngrams in zip(*[word_n[i:] for i in range(NGRAM)]):
-        rez.append(ngrams)
         print(ngrams)
-        word_dickt[ngrams]=count_ngram(ngrams,ngram)
+        rez.append(ngrams)
+        word_dickt[ngrams] = count_ngram(ngrams, ngram)
     return word_dickt
 
 
-def count_ngram(word,ngram):
-    sum=0
+def dict_word_p(ngram):
+    word_dict = defaultdict(lambda: 0.0)
     for i in ngram:
-        if i[len(i)-len(word):] == word:
-           sum+=1
+        word_dict[tuple(i)] = laplace(i, 0.2, all_count_words, ngram)
+    return word_dict
+
+
+def count_ngram(word, ngram):
+    sum = 0
+    for i in ngram:
+        if i[len(i) - len(word):] == word:
+            sum += 1
     return sum
 
 
-
-
-def laplace(word, alpha:float,v:int,ngram):
-    word_count_1=count_ngram(word,ngram)
-    word_count_2=count_ngram(word[:-1],ngram)
-    return (word_count_1+alpha)/(word_count_2+alpha*v)
+def laplace(word, alpha: float, v: int, ngram):
+    word_count_1 = count_ngram(word, ngram)
+    word_count_2 = count_ngram(word[:-1], ngram)
+    return (word_count_1 + alpha) / (word_count_2 + alpha * v)
     # return (count_ngram(" ".join(word),ngram)+alpha)/(count_ngram(" ".join(word[:-1]),ngram)+alpha*v)
-
 
 
 # def train(corp: list[str], n: int):
@@ -150,39 +154,85 @@ def p_count_word2(word1: str, word2: str, corp: list[str]):
     return rez
 
 
-
-
 test_word = "Пушкин.txt"
-test_word = "1.txt"
+test_word = "2.txt"
 # test_word = "Толстой.txt"
 # print(find_word(test_word, corp_text, 1))
-corpr, words,all_words,all_count_words= read_txt(test_word)
+corpr, words, all_words, all_count_words = read_txt(test_word)
 # print(all_count_words)
 # print(corpr)
 # print(words)
 # print(n_gram(words))
-ngram=n_gram(words)
+ngram = n_gram(words)
 print(ngram)
-word=find_word_count("This is the malt",ngram)
+word = find_word_count("This is the malt", ngram)
 print(word)
 # print(count_ngram(('$', '$', 'когда'),ngram))
 # print(count_ngram(('$', 'когда'),ngram))
 # wordd=('$', '$', 'когда')
 # wordd=('$', '$', 'когда')
-wordd=('this', 'is','the','house')
-print(laplace(wordd,0.2,all_count_words,ngram))
+wordd = ['$', '$', '$', 'this']
+
+print(laplace(wordd, 0.0, all_count_words, ngram))
 
 
+def word_ty_token(rez: str):
+    # разбитие на токены и добавление $
+    rez = "$ " * (NGRAM - 1) + rez.lower()
+    rezult_words = rez.split()
+    return rezult_words
 
 
+def word_ty_n_gram(corp: list[str]):
+    # разбитие на n gram
+    rez = []
+    for ngrams in zip(*[corp[i:] for i in range(NGRAM)]):
+        rez.append(list(ngrams))
+    return rez
+
+
+def train(word, gen: int):
+    word_token = word_ty_token(word)
+    word_ngram = word_ty_n_gram(word_token)
+    print(word_ngram)
+    word_p = laplace(word_ngram[-1], 0.0, all_count_words, ngram)
+    word_3 = word_ngram[-1]
+    max_p = 0
+    word_p_gen = 0
+    word_gen = []
+    next_gen_word = ""
+    for i in word_3[1:]:
+        word_gen.append(i)
+    for j in range(gen):
+        print('w')
+        print(word_3[-3:])
+        for i in ngram:
+            if i[:3] == word_3[-3:]:
+                word_p_gen = laplace(i, 0.0, all_count_words, ngram)
+                if max_p < word_p_gen:
+                    max_p = word_p_gen
+                    next_gen_word = i[-1]
+        max_p=0
+        word_p_gen=0
+        word_gen.append(next_gen_word)
+        word_3 = word_gen
+        print("gen:{e}{gen}".format(e=j, gen=word_gen))
+
+    print("начальное слово= " + word + " p=" + str(word_p))
+
+    print(word_gen)
+
+
+# train("This is the",5)
+# train("That", 5)
+train("была", 10)
+# print(dict_word_p(ngram))
+
+#
 # words_p={}
 # for i in ngram:
 #     words_p[i]=laplace(i,0.2,all_count_words,ngram)
 # print(words_p)
-
-
-
-
 
 
 # train(corp, 3)
